a0136e2f7efae3059db05162c215b3973a746abe
Anthony Licata <tamamaroro@gmail.com>
2017-09-19 20:36:31 -0400

    Added (most of the) music, the stem management script/prefab, and the stem switching script/prefab

---
+++ EGD1/Assets/Scripts/Audio/AudioTrigger.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class AudioTrigger : MonoBehaviour {
+
+    public AudioCommand[] commands;
+
+    bool triggered = false;
+    MusicManager manager;
+
+    private void Start()
+    {
+        manager = GameObject.Find("MusicManager").GetComponent<MusicManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!triggered)
+        {
+            foreach (AudioCommand command in commands)
+            {
+                switch (command.action)
+                {
+                    case AudioAction.Play:
+                        manager.play(command.stem);
+                        break;
+                    case AudioAction.Stop:
+                        manager.stop(command.stem);
+                        break;
+                    case AudioAction.FadeIn:
+                        manager.fadeIn(command.stem, command.duration);
+                        break;
+                    case AudioAction.FadeOut:
+                        manager.fadeOut(command.stem, command.duration);
+                        break;
+                    default:
+                        manager.stop(command.stem);
+                        break;
+                }
+            }
+            triggered = true;
+        }
+    }
+}
+
+public enum AudioAction { Play, Stop, FadeIn, FadeOut };
+
+[System.Serializable]
+public class AudioCommand
+{
+    public int stem;
+    public AudioAction action;
+    public float duration;
+}
---
+++ EGD1/Assets/Scripts/Audio/MusicManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public Stem[] stems;
+    public AudioSource[] sources;
+
+	// Use this for initialization
+	void Start () {
+        sources = new AudioSource[stems.Length];
+		for (int i = 0; i < stems.Length; i++)
+        {
+            sources[i] = gameObject.AddComponent<AudioSource>();
+            sources[i].clip = stems[i].clip;
+        }
+	}
+
+    public void play(int stem)
+    {
+        bool firstStem = true;
+        int playingStem = -1;
+        for(int i = 0; i < sources.Length; i++)
+        {
+            if (sources[i].isPlaying)
+            {
+                firstStem = false;
+                playingStem = i;
+                break;
+            }
+        }
+        if (firstStem)
+        {
+            sources[stem].volume = 1f;
+            sources[stem].Play();
+        } else
+        {
+            sources[stem].timeSamples = sources[playingStem].timeSamples;
+            sources[stem].volume = 1f;
+            sources[stem].Play();
+        }
+    }
+
+    public void stop(int stem)
+    {
+        sources[stem].Stop();
+        sources[stem].timeSamples = 0;
+    }
+
+    public void fadeIn(int stem, float fadeLength)
+    {
+        StartCoroutine(Fade(sources[stem], 1f, fadeLength));
+    }
+
+    public void fadeOut(int stem, float fadeLength)
+    {
+        StartCoroutine(Fade(sources[stem], 0f, fadeLength));
+    }
+    static IEnumerator Fade(AudioSource source, float targetVolume, float fadeLength)
+    {
+        float volDiff = Mathf.Abs(source.volume - targetVolume);
+        if(targetVolume > source.volume)
+        {
+            while(source.volume < targetVolume)
+            {
+                source.volume += volDiff * (Time.deltaTime / fadeLength);
+                yield return null;
+            }
+        } else
+        {
+            while (source.volume > targetVolume)
+            {
+                source.volume -= volDiff * (Time.deltaTime / fadeLength);
+                yield return null;
+            }
+        }
+    }
+
+}
+
+[System.Serializable]
+public class Stem
+{
+    public AudioClip clip;
+    public int length;
+}
